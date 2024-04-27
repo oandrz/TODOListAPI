db2340ffc9eb1b40f47e76739b09e671ef7d2c6b
oandrz <oentoro.andreas@gmail.com>
2024-04-27 19:52:34 +0800

    Add Docker File

--- main.py
+++ main.py
@@ -185,4 +185,4 @@ def logout():
     return jsonify(status_code=200, response={"message": "Successfully Logout"}), 200
 
 if __name__ == "__main__":
-    application.run(debug=True)
+    application.run(host="0.0.0.0", port=5000, debug=True)
